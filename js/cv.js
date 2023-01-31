b133e34f45565ee374e54f668e13ca1a95517349
Barbara Ayala <barby.ayala062@gmail.com>
2023-01-31 11:14:27 -0300

    subo proyecto

---
+++ js/cv.js
@@ -0,0 +1,63 @@
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function (data) {
+        console.log(data);
+        window.datosCV = {
+            name: data.results[0].name.first,
+            mail: data.results[0].email,
+            nacimiento: data.results[0].dob.date,
+            ubicacion: data.results[0].location.street.name,
+            fotocv: data.results[0].picture.large,
+            telefono: data.results[0].phone
+        }
+        document.getElementById('foto-cv').src = data.results[0].picture.large;
+
+        console.log(window.datosCV);
+    }
+});
+
+window.modoDark = {
+    activado: true
+}
+
+function obtenerNombre() {
+    document.getElementById('titulo').innerHTML = 'Hola! Mi nombre es:';
+    document.getElementById('dato').innerHTML = window.datosCV.name;
+}
+
+function obtenerMail() {
+    document.getElementById('titulo').innerHTML = 'mail:';
+    document.getElementById('dato').innerHTML = window.datosCV.mail;
+}
+
+function obtenerFecha() {
+    document.getElementById('titulo').innerHTML = 'Fecha de cumpleaños:';
+    document.getElementById('dato').innerHTML = window.datosCV.nacimiento.substring(0, 10);
+}
+
+function obtenerdireccion() {
+    document.getElementById('titulo').innerHTML = 'Direccion:';
+    document.getElementById('dato').innerHTML = window.datosCV.ubicacion;
+}
+
+function obtenerTelefono() {
+    document.getElementById('titulo').innerHTML = 'Telefono de contacto:';
+    document.getElementById('dato').innerHTML = window.datosCV.telefono;
+}
+
+function setDarkMode() {
+    var body = document.getElementById('body');
+    if (window.modoDark.activado == false) {
+        body.style.background = '#323334';
+        body.style.color = 'white';
+        window.modoDark.activado = true;
+
+    }
+    else {
+        body.style.background = 'white';
+        window.modoDark.activado = false;
+        body.style.color = 'black';
+    }
+}
+
